94b1a8cb3b968adb0c4ca0168e46f42847ef9502
scary <scary@scary.com>
2023-06-08 09:31:05 +0300

    startup configuration

---
+++ business_card/configuration.py
@@ -0,0 +1,54 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault
+from aiogram.methods.base import TelegramMethod, Request
+from typing import Any
+
+BOT_DESCRIPTION = \
+"This bot can just fuck you🖕🏿\n" \
+"If you want some fuck press /start"
+
+BOT_COMANDS = [
+    BotCommand(
+        command='start',
+        description='Начало работы'
+    ),
+    BotCommand(
+        command='help',
+        description='Помощь'
+    ),
+    BotCommand(
+        command='cancel',
+        description='Сбросить'
+    )
+]
+
+
+class SetMyDescription(TelegramMethod[bool]):
+    """
+    Use this method to change the bot's description, which is shown in the chat with the bot if the chat is empty. Returns True on success.
+
+    Source: https://core.telegram.org/bots/api#setchatdescription
+    """
+
+    __returning__ = bool
+
+    description: str|None = None
+    """New bot description; 0-512 characters. Pass an empty string to remove the dedicated description for the given language."""
+    language_code: str|None = None
+    """A two-letter ISO 639-1 language code. If empty, the description will be applied to all users for whose language there is no dedicated description."""
+
+    def build_request(self, bot: Bot) -> Request:
+        data: dict[str, Any] = self.dict()
+        return Request(method="setMyDescription", data=data)
+
+async def set_commands(bot: Bot):
+    commands = BOT_COMANDS
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+
+async def set_description(bot: Bot):
+    await bot(SetMyDescription(description=BOT_DESCRIPTION))
+    
+    
+async def configure(bot: Bot):
+    await set_commands(bot)
+    await set_description(bot)
