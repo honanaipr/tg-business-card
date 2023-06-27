10c657fa85d9ab98d2643fe09263376d4e1f8677
scary sources <scarysources@tutanota.org>
2023-06-27 10:37:07 +0300

    approve configuration

--- business_card/config.py
+++ business_card/config.py
@@ -1,26 +1,32 @@
-from dotenv import load_dotenv
-import environ
-from attr.validators import matches_re
 from traceback import print_exception
+
+import environ
+from attr.validators import matches_re, optional
+from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN_RE = "[0-9]{8,10}:[a-zA-Z0-9_-]{35}"
 REDIS_URL_RE = "redis://.+:[0-9]+/.+"
+HOSTNAME_RE = ".+"
+PORT_RE = "[0-9]+"
+
+vault = environ.secrets.VaultEnvSecrets(vault_prefix="BOT")
 @environ.config(prefix="")
 class AppConfig():
     @environ.config(prefix="BOT")
     class BotConfig():
-        token = environ.var(help="Telegram bot's token", validator=matches_re(TOKEN_RE))
+        token = vault.secret(help="Telegram bot's token")
     @environ.config(prefix="REDIS")
     class RedisConfig():
-        url = environ.var(help="Redis url", validator=matches_re(REDIS_URL_RE))
+        url = environ.var(help="Redis url", validator=optional(matches_re(REDIS_URL_RE)), default=None)
+        host = environ.var(help="Redis host", validator=optional(matches_re(HOSTNAME_RE)), default=None)
+        port = environ.var(help="Redis port", validator=optional(matches_re(PORT_RE)), default=None)
     bot = environ.group(BotConfig)
     redis = environ.group(RedisConfig)
 
 try:
     config = environ.to_config(AppConfig)
-    print(config)
 except Exception as e:
     print_exception(e)
     print("\n",environ.generate_help(AppConfig), "\n")
--- business_card/loader.py
+++ business_card/loader.py
@@ -1,7 +1,24 @@
-from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+from loguru import logger
+
 from business_card.config import config
 
-storage = RedisStorage.from_url(config.redis.url, key_builder=DefaultKeyBuilder(with_destiny=True))
+# if config.TELEGRAM_TEST_SERVER:
+#     bot.server = TELEGRAM_TEST
+# TODO test server
+
+if config.redis.host and config.redis.port:
+    from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+    storage = RedisStorage(config.redis.host, config.redis.port, key_builder=DefaultKeyBuilder(with_destiny=True))
+    logger.info(f"redis host: {config.redis.host}, redis port: {config.redis.port}")
+elif config.redis.url:
+    from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+    storage = RedisStorage.from_url(config.redis.url, key_builder=DefaultKeyBuilder(with_destiny=True))
+    logger.info(f"redis url: {config.redis.url}")
+else:
+    from aiogram.fsm.storage.memory import MemoryStorage
+    storage = MemoryStorage()
+
 bot = Bot(token=config.bot.token)
 dp = Dispatcher(storage=storage)
